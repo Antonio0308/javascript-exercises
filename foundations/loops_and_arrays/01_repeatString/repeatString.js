0a9ec83b645bb8e47e1fec7be881132df54a9228
Antonio0308 <179520221+Antonio0308@users.noreply.github.com>
2026-09-16 14:09:13 +0300

    Implement repeatString exercise in loops section
    - Add function logic to repeat string given number of times
    - Pass all unit test suites with Jest

--- foundations/loops_and_arrays/01_repeatString/repeatString.js
+++ foundations/loops_and_arrays/01_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function (str, num) {
+  let strRepeat = "";
+  if (num < 0) {
+    return "ERROR";
+  } else {
+    for (let i = 0; i < num; i++) {
+      strRepeat += str;
+    }
+  }
 
+  return strRepeat;
 };
 
 // Do not edit below this line
