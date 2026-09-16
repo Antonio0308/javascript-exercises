b62f57504f7ac0fc1f349f4d55c94bdc9076292a
Antonio0308 <179520221+Antonio0308@users.noreply.github.com>
2026-09-16 15:48:27 +0300

    Implement tempConversion exercise in loops section
    - Add functions to convert temperatures between Celsius and Fahrenheit rounded to one decimal place
    - Pass all unit test suites with Jest

--- foundations/loops_and_arrays/06_tempConversion/tempConversion.js
+++ foundations/loops_and_arrays/06_tempConversion/tempConversion.js
@@ -1,11 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  const temp = ((fahrenheit - 32) * 5) / 9;
+  if (Number.isInteger(temp)) {
+    return temp;
+  } else {
+    return Number(temp.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  const temp = (celsius * 9) / 5 + 32;
+  if (Number.isInteger(temp)) {
+    return temp;
+  } else {
+    return Number(temp.toFixed(1));
+  }
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
