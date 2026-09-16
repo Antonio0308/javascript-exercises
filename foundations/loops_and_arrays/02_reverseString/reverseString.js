66a07c555ccd0a5caf0a603165db030fb9d0401e
Antonio0308 <179520221+Antonio0308@users.noreply.github.com>
2026-09-16 14:18:41 +0300

    Implement reverseString exercise in loops section
    - Add function logic to reverse string
    - Pass all unit test suites with Jest

--- foundations/loops_and_arrays/02_reverseString/reverseString.js
+++ foundations/loops_and_arrays/02_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  const strLength = str.length - 1;
+  let strReverse = "";
+  for (let i = strLength; i >= 0; i--) {
+    strReverse += str[i];
+  }
+  return strReverse;
 };
 
 // Do not edit below this line
