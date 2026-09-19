7a9736fd9722c95710b662e2f0ff721e6dc2ab93
Antonio0308 <179520221+Antonio0308@users.noreply.github.com>
2026-09-19 17:31:14 +0300

    Implement palindrome function in 02_palindromes exercise
    - Process input strings by removing punctuation, spaces, and casing
    - Compare normalized input against its reversed version
    - Pass all associated Jest unit tests

--- foundations/object_basics/02_palindromes/palindromes.js
+++ foundations/object_basics/02_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let strArray = [];
+  const listWords = [" ", ",", ".", "!"];
+  const strLength = str.length - 1;
+  for (let i = strLength; i >= 0; i--) {
+    if (listWords.includes(str[i])) {
+      continue;
+    } else {
+      strArray.push(str[i].toLowerCase());
+    }
+  }
+  const palindrome = strArray.join("");
+  const newStr = strArray.reverse().join("");
+  if (palindrome === newStr) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
