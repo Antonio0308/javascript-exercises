122d88728cb6720359e7a9c2af72f7e08135db29
Antonio0308 <179520221+Antonio0308@users.noreply.github.com>
2026-09-20 22:03:33 +0300

    Implement fibonacci function in 03_fibonacci exercise
    - Compute Nth member of Fibonacci sequence starting from 1
    - Support string numeric arguments and reject negative numbers
    - Pass all associated Jest unit tests

--- foundations/object_basics/03_fibonacci/fibonacci.js
+++ foundations/object_basics/03_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (isNaN(num) || num < 0) {
+    return "OOPS";
+  } else if (Number(num) === 0) {
+    return 0;
+  }
+  let fibonacci = [1];
+  let i = 1;
+  while (i < num) {
+    if (fibonacci.length === 1) {
+      fibonacci.push(i);
+    } else {
+      const indexLastElement1 = fibonacci.length - 1;
+      const indexLastElement2 = fibonacci.length - 2;
+      const newElement =
+        fibonacci[indexLastElement1] + fibonacci[indexLastElement2];
+      fibonacci.push(newElement);
+    }
+    i++;
+  }
+  const lastNumFibonacci = fibonacci.length - 1;
+  return fibonacci[lastNumFibonacci];
 };
 
 // Do not edit below this line
