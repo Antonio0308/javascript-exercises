bad6e5072aca7f9d5afde954b5b8a19cce34849e
Antonio0308 <179520221+Antonio0308@users.noreply.github.com>
2026-09-22 23:01:54 +0300

    Implement getTheTitles function in 11_getTheTitles exercise
    - Extract book titles from array of objects using a standard for loop
    - Return new array containing title property of each object
    - Pass all associated Jest unit tests

--- foundations/object_basics/04_getTheTitles/getTheTitles.js
+++ foundations/object_basics/04_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (arr) {
+  const arrLength = arr.length;
+  const arrTitles = [];
+  for (let i = 0; i < arrLength; i++) {
+    arrTitles.push(arr[i].title);
+  }
+  return arrTitles;
 };
 
 // Do not edit below this line
