993e17d82089375b47436796b37b1809262ad402
Antonio0308 <179520221+Antonio0308@users.noreply.github.com>
2026-09-22 23:41:38 +0300

    Implement findTheOldest function in 05_findTheOldest exercise
    - Calculate age based on year of birth and year of death
    - Handle living persons by using current year when year of death is missing
    - Return oldest person from list
    - Pass all associated Jest unit tests

--- foundations/object_basics/05_findTheOldest/findTheOldest.js
+++ foundations/object_basics/05_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  const date = new Date();
+  const newArr = people.map((item) => {
+    if (item.yearOfBirth && item.yearOfDeath) {
+      return {
+        name: item.name,
+        yearOld: item.yearOfDeath - item.yearOfBirth,
+      };
+    } else {
+      return {
+        name: item.name,
+        yearOld: date.getFullYear() - item.yearOfBirth,
+      };
+    }
+  });
+  const newArrSorts = newArr.toSorted((a, b) => b.yearOld - a.yearOld);
+  return newArrSorts[0];
 };
 
 // Do not edit below this line
